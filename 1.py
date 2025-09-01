2fb9fb94a482945a4cdcb5d99cb3c18e84dcadf7
isescem <office.ise@sahyadri.edu.in>
2025-09-01 12:08:57 +0530

    initial

---
+++ 1.py
@@ -0,0 +1,31 @@
+# name=input("Enter your name:\n")
+# print(f"hello {name}")
+
+
+# import sys
+# for line in sys.stdin:
+#     print("You entered",line.strip())
+
+# import sys
+# sys.stdout.write("Enter your name");
+# name=sys.stdin.readline().strip()
+# sys.stdout.write(f"\nHello my name is {name}\n")
+
+
+# name="@Amruth"
+# Oname=name.strip("@")
+# print("Before strip:",name)
+# print("After Strip:",Oname)
+
+
+
+
+import sys
+name=input("Enter your name:")
+print("My name is",name)
+print("The length of name is:",len(name))
+for i in range(1,len(name)+1):
+    print(i)
+
+   
+
